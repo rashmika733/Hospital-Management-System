5c30146801e0288457ddf87ed887527265d32fb7
rashmika733 <adrsandeepani@gmail.com>
2026-07-23 11:48:23 +0530

    update patient java code

--- Backend/src/main/java/com/hpms/model/Patient.java
+++ Backend/src/main/java/com/hpms/model/Patient.java
@@ -30,59 +30,73 @@ public class Patient {
 
     // MongoDB _id
     public String getId() {
+
         return id;
     }
 
     public void setId(String id) {
+
         this.id = id;
     }
 
     // Custom Patient ID (P001, P002...)
     public String getPatientId() {
+
         return patientId;
     }
 
     public void setPatientId(String patientId) {
+
         this.patientId = patientId;
     }
 
     public String getPatientName() {
+
         return patientName;
     }
 
     public void setPatientName(String patientName) {
+
         this.patientName = patientName;
     }
 
     public int getAge() {
+
         return age;
     }
 
     public void setAge(int age) {
+
         this.age = age;
     }
 
     public String getGender() {
+
         return gender;
     }
 
     public void setGender(String gender) {
+
         this.gender = gender;
     }
 
     public String getPhoneNumber() {
+
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
+
         this.phoneNumber = phoneNumber;
     }
 
     public String getAddress() {
+
         return address;
     }
 
     public void setAddress(String address) {
+
         this.address = address;
     }
 }
